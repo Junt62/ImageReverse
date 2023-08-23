dd61c67b7009898727659e00f4b4d33bfbcdb661
Junt62 <junt62@qq.com>
2023-08-23 12:39:14 +0800

    更新标题

--- ImageReverse.py
+++ ImageReverse.py
@@ -6,7 +6,7 @@ from ImageReverse_logic import MainForm
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainForm()
-    window.setWindowTitle("序列帧图片翻转工具v1.0 by_zijun2023")
+    window.setWindowTitle("序列帧图片翻转工具v1.1 by_zijun2023")
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("Picture Reverse")
     window.show()
     sys.exit(app.exec_())
